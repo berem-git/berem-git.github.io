f7de24f09642a53721cc3e9cd6be64bc3e57dd46
Berembek <berembek05@gmail.com>
2020-06-26 14:09:33 +0600

    Initian commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-  $('.header-burger').click(function (event) {
-    $('.header-burger, .header__menu').toggleClass('activ');
-    $('body').toggleClass('lock');
+  $(".header-burger").click(function (event) {
+    $(".header-burger, .header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+  $(".header__list").click(function (event) {
+    $(".header-burger,.header__menu").removeClass("active");
+    $("body").removeClass("lock");
   });
 });
